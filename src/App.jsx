71fc89d7c327e19477e6de3d3a76db26bb0ba359
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-05-31 09:09:45 +0300

    add event to button

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.scss';
+
+import GoodButton from './GoodButton.jsx';
+
+const App = () => {
+  return <GoodButton />
+}
+
+export default App;
---
+++ src/GoodButton.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './goodButton.scss';
+
+class GoodButton extends Component {
+  handleClick(event) {
+    alert(event.target.textContent);
+  }
+  render() {
+    return (
+      <button 
+        className='fancy-button'
+        onClick={this.handleClick}
+      >
+        Click me!
+      </button>
+    )
+  }
+};
+
+export default GoodButton;
+
